6dca2797b5699e61e744d08350a9fce999223a48
Glitch (understood-pacific-artichoke) <none>
2024-03-30 15:27:26 +0000

    🙋🐗 Checkpoint ./index.js:60282313/1139

--- index.js
+++ index.js
@@ -22,27 +22,34 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
   let inp=req.params.date
-  console.log(inp)
+  console.log(inp+" input")
   //Fri, 25 Dec 2015 00:00:00 GMT
-  
   if(!isNaN(new Date(inp))){//date is valid
-    let unix=parseInt((new Date().getTime()));
-    res.json({ "unix" : unix ,"utc":(new Date(inp)).toUTCString()})
+    let uni=parseInt((new Date(inp).getTime()));
+    res.json({ unix : uni ,"utc":(new Date(inp)).toUTCString()})
     
   }
   else if(!isNaN(parseInt(inp))){
     //unix format
     let date=(new Date(parseInt(inp)));
     
-    res.json({ "unix" : parseInt(inp) ,"utc": date.toUTCString()})
+    res.json({unix : parseInt(inp) ,"utc": date.toUTCString()})
     
   }
+  
   else{
     
     res.json({ error : "Invalid Date" })
   }
   
 });
+
+app.get("/api", function (req, res) {
+  
+    let uni=parseInt((new Date().getTime()));
+    res.json({ unix : uni ,"utc":(new Date()).toUTCString()})
+  
+})
 console.log(new Date(1451001600000).toUTCString())
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
